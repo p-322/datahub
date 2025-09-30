40a602fae9571df2a23179f7bc0509f72f2108ba
Barbara Honhoff <barbara@honhoff.dev>
2025-09-30 11:20:10 +0200

    feat(playwright): update reporter configuration for CI environment

--- packages/playwright-config/index.ts
+++ packages/playwright-config/index.ts
@@ -12,7 +12,7 @@ export default function getBaseConfig({devBaseUrl}: {devBaseUrl: string}) {
     // Opt out of parallel tests on CI.
     workers: env.CI ? 1 : undefined,
     // Reporter to use. See https://playwright.dev/docs/test-reporters
-    reporter: 'html',
+    reporter: env.CI ? 'list' : 'html',
     // Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions.
     use: {
       // Base URL to use in actions like `await page.goto('/')`.
